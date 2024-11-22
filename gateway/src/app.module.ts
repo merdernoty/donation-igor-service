0753b037251fc0be39dffbc989ff74fed0326c87
Igor Zimin <133769530+merdernoty@users.noreply.github.com>
2024-11-23 01:57:17 +0300

    api gatewat and start proto file

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AuthController } from './controllers/auth.controller';
+import { DonationController } from './controllers/donation.controller';
+import { UserController } from './controllers/user.controller';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'auth',
+          protoPath: join(__dirname, 'proto/auth.proto'),
+        },
+      },
+      {
+        name: 'DONATION_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'donation',
+          protoPath: join(__dirname, 'proto/donation.proto'),
+        },
+      },
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'user',
+          protoPath: join(__dirname, 'proto/user.proto'),
+        },
+      },
+    ]),
+  ],
+  controllers: [AuthController, DonationController, UserController],
 })
 export class AppModule {}
